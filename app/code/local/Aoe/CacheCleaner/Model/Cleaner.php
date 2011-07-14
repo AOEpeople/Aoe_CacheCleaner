e1de69f33cef87627af585bcf9a6df292a296132
Fabrizio Branca <mail@fabrizio-branca.de>
2011-07-14 22:53:36 +0200

    Added support for some more cache commands

--- app/code/local/Aoe/CacheCleaner/Model/Cleaner.php
+++ app/code/local/Aoe/CacheCleaner/Model/Cleaner.php
@@ -21,5 +21,59 @@ class Aoe_CacheCleaner_Model_Cleaner {
 		$duration = time() - $startTime;
 		Mage::log('[CACHECLEANER] Cleaning cache (duration: '.$duration.')');
 	}
+	
+	/**
+	 * Flush Magento Cache
+	 *  
+	 * @see app/code/core/Mage/Adminhtml/controllers/CacheController.php
+	 * @return void
+	 */
+	public function flushSystem() {
+		$startTime = time();
+		Mage::app()->cleanCache();
+		$duration = time() - $startTime;
+		Mage::log('[CACHECLEANER] Flush Magento Cache (duration: '.$duration.')');
+	}
+	
+	/**
+	 * Flush all
+	 *  
+	 * @see app/code/core/Mage/Adminhtml/controllers/CacheController.php
+	 * @return void
+	 */
+	public function flushAll() {
+		$startTime = time();
+		Mage::app()->getCacheInstance()->flush();
+		$duration = time() - $startTime;
+		Mage::log('[CACHECLEANER] Flush Cache Storage (duration: '.$duration.')');
+	}
+	
+	/**
+	 * Clean images
+	 *  
+	 * @see app/code/core/Mage/Adminhtml/controllers/CacheController.php
+	 * @return void
+	 */
+	public function cleanImages() {
+		$startTime = time();
+		Mage::getModel('catalog/product_image')->clearCache();
+		Mage::dispatchEvent('clean_catalog_images_cache_after');
+		$duration = time() - $startTime;
+		Mage::log('[CACHECLEANER] Flush Catalog Images Cache (duration: '.$duration.')');
+	}
+	
+	/**
+	 * Clean media
+	 *  
+	 * @see app/code/core/Mage/Adminhtml/controllers/CacheController.php
+	 * @return void
+	 */
+	public function cleanMedia() {
+		$startTime = time();
+		Mage::getModel('core/design_package')->cleanMergedJsCss();
+		Mage::dispatchEvent('clean_media_cache_after');
+		$duration = time() - $startTime;
+		Mage::log('[CACHECLEANER] Flush JavaScript/CSS Cache (duration: '.$duration.')');
+	}
 
 }
